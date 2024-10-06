088b5530d66e6acbc3745a945457c8d4f21b4708
jjeongminchoi <chivalry1107@gmail.com>
2024-10-06 19:36:47 +0900

    first commit

---
+++ src/test/java/com/hanghae/concert_reservation/ConcertReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanghae.concert_reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcertReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
