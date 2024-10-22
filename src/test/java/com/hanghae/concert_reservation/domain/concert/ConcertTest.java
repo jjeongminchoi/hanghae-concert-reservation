54d8ea9c12be5eef4d459e9b8fe6022ccffcd9c9
jjeongminchoi <chivalry1107@gmail.com>
2024-10-22 23:26:28 +0900

    test: 예약 & 유저포인트 관련 단위 테스트 추가

--- src/test/java/com/hanghae/concert_reservation/domain/concert/ConcertTest.java
+++ src/test/java/com/hanghae/concert_reservation/domain/concert/ConcertTest.java
@@ -1,5 +1,6 @@
 package com.hanghae.concert_reservation.domain.concert;
 
+import com.hanghae.concert_reservation.common.exception.BizInvalidException;
 import com.hanghae.concert_reservation.domain.concert.constant.ConcertSeatStatus;
 import com.hanghae.concert_reservation.domain.concert.constant.ReservationStatus;
 import com.hanghae.concert_reservation.domain.concert.entity.ConcertSeat;
@@ -26,6 +27,23 @@ class ConcertTest {
     }
 
     @Test
+    void setToTemporaryReservationTime_Should_SetTempReservedAtAndUpdatedAt() {
+        // given
+        LocalDateTime now = LocalDateTime.now();
+        Reservation reservation = Reservation.of(1L, 1L, "아이유콘서트", now, BigDecimal.valueOf(100000));
+
+        // when
+        reservation.setToTemporaryReservationTime();
+
+        // then
+        assertThat(reservation.getTempReservedAt()).isAfter(now.plusMinutes(4));
+        assertThat(reservation.getTempReservedAt()).isBefore(now.plusMinutes(6));
+
+        assertThat(reservation.getUpdatedAt()).isAfter(now);
+        assertThat(reservation.getUpdatedAt()).isBefore(now.plusMinutes(1));
+    }
+
+    @Test
     void changeReservationStatus() {
         // given
         Reservation reservation = Reservation.of(1L, 1L, "아이유콘서트", LocalDateTime.now(), BigDecimal.valueOf(100000));
@@ -35,5 +53,17 @@ class ConcertTest {
 
         // then
         assertThat(reservation.getReservationStatus()).isEqualTo(ReservationStatus.EXPIRED);
+    }
+
+    @Test
+    void isAvailableSeat() {
+        // given
+        ConcertSeat concertSeat = ConcertSeat.of(1L, 1, BigDecimal.valueOf(10000));
+        concertSeat.changeConcertSeatStatus(ConcertSeatStatus.TEMPORARILY_RESERVED);
+
+        // exception
+        assertThatThrownBy(concertSeat::isAvailable)
+                .isInstanceOf(BizInvalidException.class)
+                .hasMessageContaining("예약할 수 없는 좌석입니다");
     }
 }
--- src/test/java/com/hanghae/concert_reservation/domain/user/UserPointTest.java
+++ src/test/java/com/hanghae/concert_reservation/domain/user/UserPointTest.java
@@ -1,6 +1,7 @@
 package com.hanghae.concert_reservation.domain.user;
 
 import com.hanghae.concert_reservation.common.exception.BizIllegalArgumentException;
+import com.hanghae.concert_reservation.common.exception.BizInvalidException;
 import com.hanghae.concert_reservation.domain.user.entity.UserPoint;
 import org.junit.jupiter.api.Test;
 
@@ -38,7 +39,7 @@ class UserPointTest {
     }
 
     @Test
-    void chargeUserPointWithNegativeAmount() {
+    void should_ThrowException_When_chargeUserPointWithNegativeAmount() {
         // given
         UserPoint userPoint = UserPoint.of(1L, BigDecimal.valueOf(10000));
         BigDecimal chargeAmount = BigDecimal.valueOf(0);
@@ -49,5 +50,25 @@ class UserPointTest {
                 .hasMessageContaining("충전할 금액이 없습니다");
     }
 
+    @Test
+    void should_ThrowException_When_InsufficientBalance() {
+        // given
+        UserPoint userPoint = UserPoint.of(1L, BigDecimal.valueOf(10000));
 
+        // exception
+        assertThatThrownBy(() -> userPoint.usePoint(BigDecimal.valueOf(10001)))
+                .isInstanceOf(BizIllegalArgumentException.class)
+                .hasMessageContaining("잔액이 부족합니다");
+    }
+
+    @Test
+    void should_ThrowException_When_AmountIsZeroOrNegative() {
+        // given
+        UserPoint userPoint = UserPoint.of(1L, BigDecimal.valueOf(10000));
+
+        // exception
+        assertThatThrownBy(() -> userPoint.usePoint(BigDecimal.valueOf(0)))
+                .isInstanceOf(BizInvalidException.class)
+                .hasMessageContaining("비정상 결제 금액입니다");
+    }
 }
